7ec7f9905cc2123106496d7c2eea7c79908abf5f
Timur <www.timmy.kz@gmail.com>
2022-02-17 19:13:29 +0600

    добавлены циклы

--- script.js
+++ script.js
@@ -99,3 +99,36 @@
 // 	}
 // 	setInterval(kill, 1)
 // }
+
+// обучение js
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB = {
+	count : numberOfFilms,
+	movies : {},
+	actors : {},
+	genres : [],
+	privat : false
+};
+
+for ( let i = 0; i < 2; i++ ){
+	const   a = prompt('Один из последних просмотренных фильмов?'),
+			b = prompt('На сколько оцените его?');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50){
+			personalMovieDB.movies[a] = b;
+			console.log('yes');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+if (personalMovieDB.count <= 10){
+	console.log("мало");
+} else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+	console.log("norm");
+}else if(personalMovieDB.count <= 30){
+	console.log("kinoman");
+}
+	console.log(personalMovieDB);
